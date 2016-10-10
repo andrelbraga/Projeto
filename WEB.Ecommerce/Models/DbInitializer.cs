ff2c41a772bbed3d0ade979c250bdd62e51d9f6a
andrelbraga <dnk_vip@hotmail.com>
2016-10-10 00:33:23 -0300

    DbInitializer

--- WEB.Ecommerce/Models/DbInitializer.cs
+++ WEB.Ecommerce/Models/DbInitializer.cs
@@ -15,15 +15,15 @@ namespace WEB.Ecommerce.Models
             var categorias = new List<Categoria>
             {
                 new Categoria() { Tipo = "Informatica", Descricao = "Produtos de Tecnologia voltados a area de informatica."},
-                new Categoria() { Tipo = "Eletrodomestico", Descricao = "Produtos de eletrodomestico."},
-                new Categoria() { Tipo = "", Descricao = ""},
-                new Categoria() { Tipo = "", Descricao = ""},
-                new Categoria() { Tipo = "", Descricao = ""},
-                new Categoria() { Tipo = "", Descricao = ""},
-                new Categoria() { Tipo = "", Descricao = ""},
-                new Categoria() { Tipo = "", Descricao = ""},
-                new Categoria() { Tipo = "", Descricao = ""},
-                new Categoria() { Tipo = "", Descricao = ""},
+                new Categoria() { Tipo = "Eletrodoméstico", Descricao = "Aparelhos elétricos voltados para facilitar tarefas domsticas."},
+                new Categoria() { Tipo = "Vestuario", Descricao = "Produtos voltados para vestimenta, incluindo calçados e acessórios em geral"},
+                new Categoria() { Tipo = "Filmes e Seriados", Descricao = "Secção voltada para produtos cinematograficos entre eles fitas, DVDs e boxes"},
+                new Categoria() { Tipo = "Games", Descricao = "Produtos de jogos eletronicos "},
+                new Categoria() { Tipo = "Produtos musicais", Descricao = "Secção voltada para musica incluindo CDs,instrumentos e acessorios para instumentos"},
+                new Categoria() { Tipo = "Livros", Descricao = "Secção voltada para livros, apostila e audio book"},
+                new Categoria() { Tipo = "Moveis e Decorações", Descricao = "Secção voltada para moveis e decorações em geral"},
+                new Categoria() { Tipo = "Carro,Motos e Automoveis", Descricao = "Secção voltada para objetos detinados para automoveis "},
+                new Categoria() { Tipo = "Brinquedos e Hobbies", Descricao = "Secção voltadada para brinquedos e objetos para divesão em geral"},
             };
 
             categorias.ForEach(x => context.Categoria.Add(x));
@@ -32,7 +32,7 @@ namespace WEB.Ecommerce.Models
             //Produtos
             var produtos = new Produto()
             {
-                
+
             };
 
             //Clientes
